6ea2acebd1542ecfee52c981332ee9a943ef7c38
Oswina <30807682+Oswina@users.noreply.github.com>
2017-08-08 23:00:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var morgan = require('morgan');
+var express = require('express'); //to create the webservers
+var morgan = require('morgan'); //to output logs of our servers
 var path = require('path');
 
 var app = express();
@@ -7,6 +7,18 @@ app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/article-one', function(req, res) {
+    res.send('Article one requested and will be served here');
+});
+
+app.get('/article-two', function(req, res) {
+    res.send('Article two requested and will be served here');
+});
+
+app.get('/article-three', function(req, res) {
+    res.send('Article three requested and will be served here');
 });
 
 app.get('/ui/style.css', function (req, res) {
