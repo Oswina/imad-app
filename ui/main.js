440c10a2c8806234d00f8a4a89b000fcc77db301
Oswina <30807682+Oswina@users.noreply.github.com>
2017-08-13 23:26:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,12 @@
 console.log('Loaded!');
 
+//change text of main text division
 var element = document.getElementById('main text');
 
 element.innerHTML = 'New value';
+
+//move the image
+var img = document.getElementById('img');
+img.onclick = function() {
+    img.style.marginLeft='100px';
+};
