730e13d5167c6147ee0019a467548d335f11bd0a
Oswina <30807682+Oswina@users.noreply.github.com>
2017-08-14 23:25:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,8 +25,6 @@ button.onclick = function() {
 };
 
 //submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
     //make a request to the server and send the name
@@ -53,6 +51,8 @@ submit.onclick = function() {
   };
   
   //make the request
+  var nameInput = document.getElementById('name');
+  var name = nameInput.value;
   request.open('GET', 'http://os11196.imad.hasura-app.io/submit-name?name=' + name, true);
   request.send(null);
 
